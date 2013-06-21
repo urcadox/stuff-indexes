0e283241aa3ed7947103333c2b7aacde760a524b
Alexandre Berthaud <alexandre@berthaud.me>
2013-06-21 17:14:24 +0200

    Change: Removed useless externalizing of URLs

--- hakyll.hs
+++ hakyll.hs
@@ -2,9 +2,7 @@
 module Main where
 
 import Data.Functor ((<$>))
-import Data.List (isPrefixOf)
 import Data.Monoid (mappend, mconcat)
-import Data.Text (pack, unpack, replace, empty)
 
 import Hakyll
 
@@ -35,7 +33,6 @@ main = hakyll $ do
             list <- applyTemplateList itemTpl itemCtx images
             makeItem list
                 >>= loadAndApplyTemplate "templates/list.html" allImagesCtx
-                >>= relativizeUrls
 
     -- Render gifs index
     create ["gifs.html"] $ do
@@ -46,7 +43,6 @@ main = hakyll $ do
             list <- applyTemplateList itemTpl itemCtx gifs
             makeItem list
                 >>= loadAndApplyTemplate "templates/list.html" allGifsCtx
-                >>= relativizeUrls
 
     -- Read templates
     match "templates/*" $ compile templateCompiler
@@ -64,24 +60,3 @@ allGifsCtx :: Context String
 allGifsCtx =
     constField "title" "Gifs" `mappend`
     defaultContext
-
-externalizeUrls :: String -> Item String -> Compiler (Item String)
-externalizeUrls root item = return $ fmap (externalizeUrlsWith root) item
-
-externalizeUrlsWith :: String -- ^ Path to the site root
-                    -> String -- ^ HTML to externalize
-                    -> String -- ^ Resulting HTML
-externalizeUrlsWith root = withUrls ext
-  where
-    ext x = if isExternal x then x else root ++ x
-
--- TODO: clean me
-unExternalizeUrls :: String -> Item String -> Compiler (Item String)
-unExternalizeUrls root item = return $ fmap (unExternalizeUrlsWith root) item
-
-unExternalizeUrlsWith :: String -- ^ Path to the site root
-                      -> String -- ^ HTML to unExternalize
-                      -> String -- ^ Resulting HTML
-unExternalizeUrlsWith root = withUrls unExt
-  where
-    unExt x = if root `isPrefixOf` x then unpack $ replace (pack root) empty (pack x) else x
