e5d8317b3681ff2394d225254a55ae12bff645d9
Alexandre Berthaud <alexandre@berthaud.me>
2013-09-12 17:26:15 +0200

    Fix: Some more TypeError in Firefox é_è

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 var Main = {
   init: function() {
-    var ipp = document.getElementsByTagName("ul").item().dataset.itemsperpage,
+    var ipp = document.getElementsByTagName("ul").item(0).dataset.itemsperpage,
         that = this;
 
     var imgs = document.getElementsByClassName("content").item(0).getElementsByTagName("img");
-    document.getElementsByTagName("input").item().onkeyup = function(e) {
+    document.getElementsByTagName("input").item(0).onkeyup = function(e) {
       if(this.value.length < 3) {
         that.initialDisplay(ipp);
       } else {
-        document.getElementsByClassName("prev").item().classList.add("disabled");
-        document.getElementsByClassName("next").item().classList.add("disabled");
-        var li = document.getElementsByClassName("content").item().getElementsByTagName("li");
+        document.getElementsByClassName("prev").item(0).classList.add("disabled");
+        document.getElementsByClassName("next").item(0).classList.add("disabled");
+        var li = document.getElementsByClassName("content").item(0).getElementsByTagName("li");
         for(var i = 0; i < li.length; i++) {
           li.item(i).classList.add("hidden");
         }
@@ -28,16 +28,16 @@ var Main = {
 
     this.initialDisplay(ipp);
 
-    document.getElementsByClassName("prev").item().onclick = function(e) {
+    document.getElementsByClassName("prev").item(0).onclick = function(e) {
       e.preventDefault();
       if(this.classList.contains("disabled")) return;
 
-      var li = document.getElementsByClassName("content").item().getElementsByTagName("li");
+      var li = document.getElementsByClassName("content").item(0).getElementsByTagName("li");
       var lastDisplayedIndex = -1;
       var newlyDisplayedItems = 0;
       for(var i = li.length - 1; i >= 0; i--) {
         var item = li.item(i);
-        var img = item.getElementsByTagName("img").item();
+        var img = item.getElementsByTagName("img").item(0);
         if(!item.classList.contains("hidden")) {
           lastDisplayedIndex = i;
           item.classList.add("hidden");
@@ -51,18 +51,18 @@ var Main = {
           }
         }
       }
-      document.getElementsByClassName("next").item().classList.remove("disabled");
+      document.getElementsByClassName("next").item(0).classList.remove("disabled");
     };
-    document.getElementsByClassName("next").item().onclick = function(e) {
+    document.getElementsByClassName("next").item(0).onclick = function(e) {
       e.preventDefault();
       if(this.classList.contains("disabled")) return;
 
-      var li = document.getElementsByClassName("content").item().getElementsByTagName("li");
+      var li = document.getElementsByClassName("content").item(0).getElementsByTagName("li");
       var lastDisplayedIndex = -1;
       var newlyDisplayedItems = 0;
       for(var i = 0; i < li.length; i++) {
         var item = li.item(i);
-        var img = item.getElementsByTagName("img").item();
+        var img = item.getElementsByTagName("img").item(0);
         if(!item.classList.contains("hidden")) {
           lastDisplayedIndex = i;
           item.classList.add("hidden");
@@ -76,27 +76,27 @@ var Main = {
           }
         }
       }
-      document.getElementsByClassName("prev").item().classList.remove("disabled");
+      document.getElementsByClassName("prev").item(0).classList.remove("disabled");
     }
   },
 
   initialDisplay: function(ipp) {
-    var li = document.getElementsByClassName("content").item().getElementsByTagName("li");
+    var li = document.getElementsByClassName("content").item(0).getElementsByTagName("li");
     for(var i = 0; i < li.length; i++) {
       li.item(i).classList.add("hidden");
     }
     for(var i = 0; i < ipp && i < li.length; i++) {
       var item = li.item(i);
-      var img = item.getElementsByTagName("img").item();
+      var img = item.getElementsByTagName("img").item(0);
       if(!img.hasAttribute("src") || img.src.split("/").pop() == "#") {
         img.setAttribute("src", img.dataset.src);
       }
       item.classList.remove("hidden");
     }
     if(li.length <= ipp) {
-      document.getElementsByClassName("next").item().classList.add("disabled");
+      document.getElementsByClassName("next").item(0).classList.add("disabled");
     } else {
-      document.getElementsByClassName("next").item().classList.remove("disabled");
+      document.getElementsByClassName("next").item(0).classList.remove("disabled");
     }
   }
 };
